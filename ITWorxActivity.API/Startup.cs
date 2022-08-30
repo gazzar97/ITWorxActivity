9b9c7e93dc92d7f9bdd7918889ab2f0291fa8acc
Mahmoud Elgazzar <Mahmoud.Elgazzar@itworx.com>
2022-08-30 17:59:57 +0200

    add swagger ui functionality

--- ITWorxActivity.API/Startup.cs
+++ ITWorxActivity.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,6 +36,11 @@ namespace ITWorxActivity.API
                 );
             services.AddTransient<ICategoryRepository, CategoryRepository>();
             services.AddTransient<IProductRepository, ProductRepository>();
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ITWORXActivityAPIDOC", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +49,8 @@ namespace ITWorxActivity.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ITWORXActivityAPIDOC v1"));
             }
 
             app.UseHttpsRedirection();
